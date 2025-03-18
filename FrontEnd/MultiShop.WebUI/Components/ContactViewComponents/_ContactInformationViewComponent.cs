b2f36d6fc69d7d1b28d9fd158ba8de174bc4753a
korayciftci <korayciftciii@users.noreply.github.com>
2025-03-19 00:11:01 +0200

    Contact Configuration

---
+++ FrontEnd/MultiShop.WebUI/Components/ContactViewComponents/_ContactInformationViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Components.ContactViewComponents
+{
+    public class _ContactInformationViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FrontEnd/MultiShop.WebUI/Components/ShoppingCartViewComponents/_ShoppingCartActionSectionViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Components.ShoppingCartViewComponents
+{
+    public class _ShoppingCartActionSectionViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
