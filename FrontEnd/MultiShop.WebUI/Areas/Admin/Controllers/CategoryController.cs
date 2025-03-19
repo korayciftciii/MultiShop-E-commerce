262299cc6618a7d7c2213a40cea7c14c1af8e6f9
korayciftci <korayciftciii@users.noreply.github.com>
2025-03-19 23:27:18 +0200

    Crud operations for product and categories

--- FrontEnd/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ FrontEnd/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -3,9 +3,11 @@ using Newtonsoft.Json;
 
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Microsoft.AspNetCore.Authorization;
+using System.Text;
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -14,7 +16,8 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-       
+
+    
         public async Task<IActionResult> Index()
         {
             var client=_httpClientFactory.CreateClient();
@@ -24,6 +27,65 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 var JsonData=await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(JsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+      
+        public IActionResult NewCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NewCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var response = await client.PostAsync("https://localhost:7070/api/Category", stringContent);
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Category",new { area="Admin"});
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> DeleteCategory([FromQuery(Name="categoryId")]string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response=await client.DeleteAsync("https://localhost:7070/api/Category/"+id);
+            if(response.IsSuccessStatusCode) 
+                {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+                }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory([FromQuery(Name = "categoryId")] string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Category/"+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(JsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory([FromForm]UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7070/api/Category", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
             }
             return View();
         }
---
+++ FrontEnd/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using MultiShop.DtoLayer.CatalogDtos.ProductDto;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<List<SelectListItem>> GetCategories()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Category");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+
+                return categories.Select(c => new SelectListItem
+                {
+                    Value = c.CategoryId.ToString(),
+                    Text = c.CategoryName
+                }).ToList();
+            }
+
+            return new List<SelectListItem>(); // Hata olursa boş liste döner
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteProduct([FromQuery(Name = "productId")] string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync("https://localhost:7070/api/Product/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage =await client.GetAsync("https://localhost:7070/api/Product");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(JsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> NewProduct()
+        {
+            ViewBag.Categories = await GetCategories();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NewProduct([FromForm] CreateProductDto createProductDto)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7070/api/Product", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+ 
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct([FromQuery(Name = "productId")] string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Product/" + id);
+            if (responseMessage.IsSuccessStatusCode )
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDto>(JsonData);
+                ViewBag.Categories = await GetCategories();
+
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateProduct([FromForm] UpdateProductDto updateProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7070/api/Product", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
