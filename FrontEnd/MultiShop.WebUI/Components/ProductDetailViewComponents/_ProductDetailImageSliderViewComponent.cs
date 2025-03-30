6780c8b28f43d05c75554880edc3a35b28852f9d
korayciftci <korayciftciii@users.noreply.github.com>
2025-03-30 05:54:02 +0300

    Add product image slider and related DTOs
    - Implemented asynchronous image fetching in the
      _ProductDetailImageSliderViewComponent using
      IHttpClientFactory and added InvokeAsync method.
    
    - Updated ProductDetail.cshtml to pass ProductId for
      correct image display.
    
    - Refactored links in Default.cshtml to use ASP.NET
      Core tag helpers and added conditional rendering for
      pricing.
    
    - Introduced new DTO classes for product images:
      CreateProductImageDto, ResultProductImageDto,
      and UpdateProductImageDto.
    
    - Updated _ViewImports.cshtml to include new
      ProductImageDtos namespace.
    
    - Added new image files for the product image slider.

--- FrontEnd/MultiShop.WebUI/Components/ProductDetailViewComponents/_ProductDetailImageSliderViewComponent.cs
+++ FrontEnd/MultiShop.WebUI/Components/ProductDetailViewComponents/_ProductDetailImageSliderViewComponent.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using MultiShop.DtoLayer.CatalogDtos.ProductImageDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Linq;
+using System.Collections.Generic;
 namespace MultiShop.WebUI.Components.ProductDetailViewComponents
 {
     public class _ProductDetailImageSliderViewComponent :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailImageSliderViewComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/ProductImage/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductImageDto>>(JsonData);
+                var result=values.Where(x => x.ProductId == id).FirstOrDefault();
+                return View(result);
+            }
             return View();
         }
     }
